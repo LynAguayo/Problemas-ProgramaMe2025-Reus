48fafb2c89c8577db140fec307402ef0b0cb8158
evelnyit <125038025+evelnyit@users.noreply.github.com>
2025-03-14 16:45:24 +0100

    ejercicio B hecho

--- src/ejercicioB.java
+++ src/ejercicioB.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class ejercicioB {
+import java.util.Scanner;
+
+public class ejercicioB {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int n = -1;
+
+        int result = 1;
+
+        while (n != 0) {
+            n = input.nextInt();
+
+            if ( n != 0 ){
+                result = result * n;
+            }
+        }
+
+        System.out.println(result);
+        input.close();
+    }
 }
